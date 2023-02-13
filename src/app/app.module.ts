c4f422775d0c621421e4113e24a61c8981d85f34
ahmedshahd <ahmedshahd350@gmail.com>
2023-02-13 21:06:29 +0200

    creating ui for label

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,20 +11,10 @@ import { LoginComponent } from './login/login.component';
 import { MaterialModule } from './material.module';
 import { AuthService } from './shared/services/auth/auth.service';
 import { NotificationService } from './shared/services/notifications/notification.service';
-import { LabelListComponent } from './label/label-list/label-list.component';
-import { LabelDetailsComponent } from './label/label-details/label-details.component';
-import { TermsAndConditionsDetailsComponent } from './terms-and-conditions/terms-and-conditions-details/terms-and-conditions-details.component';
-import { TermsAndConditionsListComponent } from './terms-and-conditions/terms-and-conditions-list/terms-and-conditions-list.component';
 import { TermsAndConditionsComponent } from './terms-and-conditions/terms-and-conditions.component';
-import { WelcomeScreenListComponent } from './welcome-screen/welcome-screen-list/welcome-screen-list.component';
-import { WelcomeScreenDetailsComponent } from './welcome-screen/welcome-screen-details/welcome-screen-details.component';
 import { WelcomeScreenComponent } from './welcome-screen/welcome-screen.component';
 import { LearnIconComponent } from './learn-icon/learn-icon.component';
-import { LearnIconListComponent } from './learn-icon/learn-icon-list/learn-icon-list.component';
-import { LearnIconDetailsComponent } from './learn-icon/learn-icon-details/learn-icon-details.component';
 import { LineOfBusinessComponent } from './line-of-business/line-of-business.component';
-import { LineOfBusinessListComponent } from './line-of-business/line-of-business-list/line-of-business-list.component';
-import { LineOfBusinessDetailsComponent } from './line-of-business/line-of-business-details/line-of-business-details.component';
 import { GraphQLModule } from './graphql.module';
 import { LabelComponent } from './label/label.component';
 
@@ -44,21 +34,11 @@ import { LabelComponent } from './label/label.component';
     HomeComponent,
     LoginComponent,
     FaqComponent,
-    LabelListComponent,
-    LabelDetailsComponent,
     LabelComponent,
-    TermsAndConditionsDetailsComponent,
-    TermsAndConditionsListComponent,
     TermsAndConditionsComponent,
-    WelcomeScreenListComponent,
-    WelcomeScreenDetailsComponent,
     WelcomeScreenComponent,
     LearnIconComponent,
-    LearnIconListComponent,
-    LearnIconDetailsComponent,
     LineOfBusinessComponent,
-    LineOfBusinessListComponent,
-    LineOfBusinessDetailsComponent,
   ],
   providers: [AuthService, NotificationService],
   bootstrap: [AppComponent],
